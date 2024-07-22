86b03e4feb9eb56b13fd3ecdbcbc8ffc0e827b54
Nizwara <129714834+Nizwara@users.noreply.github.com>
2024-07-22 20:36:38 +0700

    Add files via upload

--- menu/restore.sh
+++ menu/restore.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
 colornow=$(cat /etc/rmbl/theme/color.conf)
+export NC="\e[0m"
+export YELLOW='\033[0;33m';
+export RED="\033[0;31m"
+export COLOR1="$(cat /etc/rmbl/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+export COLBG1="$(cat /etc/rmbl/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
+WH='\033[1;37m'
+###########- END COLOR CODE -##########
+ipsaya=$(curl -sS ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
 clear
 echo ""
 echo " This Feature Can Only Be Used According To VPS Data With This Autoscript"
@@ -27,7 +39,13 @@ cp -r id /etc/per/id &> /dev/null
 cp -r token2 /etc/per/token &> /dev/null
 cp -r loginid /etc/perlogin/id &> /dev/null
 cp -r logintoken /etc/perlogin/token &> /dev/null
+#cp gshadow /etc/ /etc/ &> /dev/null
+#cp -r /root/backup/.acme.sh /root/ &> /dev/null
+#cp -r /root/backup/xray /etc/ &> /dev/null
+#cp -r /root/backup/conf.d /etc/nginx/ &> /dev/null
 cp -r public_html /home/vps/ &> /dev/null
+#cp -r crontab /etc/ &> /dev/null
+#cp -r cron.d /etc/ &> /dev/null
 cp -r gshadow /etc/ &> /dev/null
 cp -r sshx /etc/xray/ &> /dev/null
 cp -r vmess /etc/ &> /dev/null
