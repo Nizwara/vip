4f0e9a9318891ba481478a2d96fdc0559e5c7c93
Nizwara <129714834+Nizwara@users.noreply.github.com>
2024-07-22 16:03:50 +0700

    Add files via upload

--- menu/restore.sh
+++ menu/restore.sh
@@ -1,38 +1,6 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-###########- COLOR CODE -##############
 colornow=$(cat /etc/rmbl/theme/color.conf)
-export NC="\e[0m"
-export YELLOW='\033[0;33m';
-export RED="\033[0;31m"
-export COLOR1="$(cat /etc/rmbl/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
-export COLBG1="$(cat /etc/rmbl/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
-WH='\033[1;37m'
-###########- END COLOR CODE -##########
-ipsaya=$(curl -sS ipinfo.io/ip)
-data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-date_list=$(date +"%Y-%m-%d" -d "$data_server")
-checking_sc() {
-    useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
-    if [[ $date_list < $useexp ]]; then
-        echo -ne
-    else
-        systemctl stop nginx
-        echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-        echo -e "$COLOR1│${NC}${COLBG1}          ${WH}• AUTOSCRIPT PREMIUM •                 ${NC}$COLOR1│ $NC"
-        echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-        echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
-        echo -e "$COLOR1│            ${RED}PERMISSION DENIED !${NC}                  $COLOR1│"
-        echo -e "$COLOR1│   ${yl}Your VPS${NC} $ipsaya \033[0;36mHas been Banned${NC}      $COLOR1│"        
-        echo -e "$COLOR1│     ${yl}Buy access permissions for scripts${NC}          $COLOR1│"
-        echo -e "$COLOR1│             \033[0;32mContact Your Admin ${NC}                 $COLOR1│"
-        echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
-        exit
-    fi
-}
-checking_sc
-
 clear
 echo ""
 echo " This Feature Can Only Be Used According To VPS Data With This Autoscript"
@@ -59,13 +27,7 @@ cp -r id /etc/per/id &> /dev/null
 cp -r token2 /etc/per/token &> /dev/null
 cp -r loginid /etc/perlogin/id &> /dev/null
 cp -r logintoken /etc/perlogin/token &> /dev/null
-#cp gshadow /etc/ /etc/ &> /dev/null
-#cp -r /root/backup/.acme.sh /root/ &> /dev/null
-#cp -r /root/backup/xray /etc/ &> /dev/null
-#cp -r /root/backup/conf.d /etc/nginx/ &> /dev/null
 cp -r public_html /home/vps/ &> /dev/null
-#cp -r crontab /etc/ &> /dev/null
-#cp -r cron.d /etc/ &> /dev/null
 cp -r gshadow /etc/ &> /dev/null
 cp -r sshx /etc/xray/ &> /dev/null
 cp -r vmess /etc/ &> /dev/null
